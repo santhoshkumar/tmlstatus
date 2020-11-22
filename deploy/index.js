927c5bc205e95505c624ee2c0511d6f6d6e0e9e1
santhoshkumar <santhoshveerblog@gmail.com>
2020-11-22 08:11:39 +0530

    Server Deploy via Ghost Admin API

---
+++ deploy/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const GhostAdminApi = require('@tryghost/admin-api');
+
+(async function main() {
+  try {
+    const api = new GhostAdminApi({
+      url: process.env.url,
+      key: process.env.key,
+      version: 'v3'
+    });
+
+    await api.themes.upload({file: '../dist/tmlstatus.zip'});
+    console.log('Theme successfully uploaded.');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}());
